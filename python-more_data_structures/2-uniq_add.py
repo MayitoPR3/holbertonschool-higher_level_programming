802f77b2fc564e8e74a37d25b0db64232202c155
Jose Mayo <josemayo3@gmail.com>
2024-05-14 23:03:25 -0400

    push unique addition

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+        Makelist = []
+        for m in my_list:
+            if m not in Makelist:
+                Makelist.append(m)
+        return sum(Makelist)
