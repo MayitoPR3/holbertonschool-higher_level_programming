27042e6de9043fd7471195f149001b30aa16bb90
Jose Mayo <josemayo3@gmail.com>
2024-05-14 23:20:16 -0400

    fixed space

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-        another_list = []
-        for i in my_list:
-            if i not in another_list:
-                another_list.append(i)
+    another_list = []
+    for i in my_list:
+        if i not in another_list:
+            another_list.append(i)
         return sum(another_list)
