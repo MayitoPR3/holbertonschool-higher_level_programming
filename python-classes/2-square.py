bc69db3dca085557543b467da2f682cc3ca3a218
Jose Mayo <josemayo3@gmail.com>
2024-05-16 18:24:48 -0400

    push task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """This module creates a class named Square"""
 class Square:
-    """above is class named Square
+    """
+    above is class named Square
 
     Atributes:
-    atr1 (size): size of square"""
+    atr1 (size): size of square
+    """
     def __init__(self, size=0):
-        """Args:
-        size (int): size for __size attribute of class instance"""
+        """
+        Args:
+        size (int): size for __size attribute of class instance
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
