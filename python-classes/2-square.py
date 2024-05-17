947e4004d588a671178abc4669354e1c6baa4f97
Jose Mayo <josemayo3@gmail.com>
2024-05-17 00:43:53 -0400

    fixing doc error

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,19 +3,21 @@
 
 
 class Square:
+
     """
     This is the class named Square
 
     Attributes:
-    
     attr1 (size): means size of square
     """
+
     def __init__(self, size=0):
+
         """
         Args:
-
         size (int): size for __size attribute of class instance
         """
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
