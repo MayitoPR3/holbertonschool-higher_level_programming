9d3eb10961896ffa38f4cefa16c379d6abed3339
Jose Mayo <josemayo3@gmail.com>
2024-05-16 23:46:27 -0400

    add documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""This module creates a class Square"""
 class Square:
+    """
+    This is the class Square
+    
+    Attributes
+    attr1: (size) size of square
+    """
     def __init__(self, size):
+        """
+        Arguments
+        size for __size attribute of class instance
+        """
         self.__size = size
