02b3c26933dd6057e62fc1fc7e72d3e24e805f47
Jose Mayo <josemayo3@gmail.com>
2024-05-21 13:56:42 -0400

    update code

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Property to set the width"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Property to set the height"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
