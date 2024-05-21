cdbdbd3f651c519effddb6b4efbcd9154bb764c6
Jose Mayo <josemayo3@gmail.com>
2024-05-21 19:48:15 -0400

    fixing errors of comments in class task 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
+        """initializes a rectangle"""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
