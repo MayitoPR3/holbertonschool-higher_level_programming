05862ffeacff8279941b1b7ebefaed28ab3a3b4d
Jose Mayo <josemayo3@gmail.com>
2024-05-21 13:28:20 -0400

    update class error

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""
-This is the module to create the class Rectangle
-"""
-
-
 class Rectangle:
     """This is the Rectangle Class"""
     def __init__(self, width=0, height=0):
@@ -26,7 +21,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Property to set the width"""
-        if type(value) != int:
+        if isinstance(value, int) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -40,7 +35,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Property to set the height"""
-        if type(value) != int:
+        if isinstance(value, int) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
