1383c3ca102b02fecab26a7a19c0b815bf43f01b
Jose Mayo <josemayo3@gmail.com>
2024-05-21 13:13:03 -0400

    push task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This is the module to create the class Rectangle
+"""
+
+
+class Rectangle():
+    """This is the Rectangle Class"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        return (self.width + self.height) * 2
+
+    @property
+    def width(self):
+        """Property to retrieve the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Property to set the width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Property to get the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Property to set the height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
