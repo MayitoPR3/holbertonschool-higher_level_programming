0f7e50c22d2605fd7124f5c02bd1e8e958847028
Jose Mayo <josemayo3@gmail.com>
2024-05-21 12:19:07 -0400

    push task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,6 +8,10 @@ class Rectangle:
     """
     This is the empty class
     """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
     @property
     def width(self):
         """
