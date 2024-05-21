0d68d68bfaf171083ec0097a4678444786c1be15
Jose Mayo <josemayo3@gmail.com>
2024-05-21 13:38:00 -0400

    update class error

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Property to set the width"""
-        if isinstance(value, int) != int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Property to set the height"""
-        if isinstance(value, int) != int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
