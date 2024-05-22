4dffbf319959293df2e629fc772c0a5fb22a16ad
Jose Mayo <josemayo3@gmail.com>
2024-05-22 18:43:15 -0400

    push task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function that lookup of and object"""
+
+
+def lookup(obj):
+    """function to return the list of available attributes
+     and methods of an obj
+     """
+    return list(obj)
