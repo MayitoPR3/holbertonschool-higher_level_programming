0308accb51b4b1f01930812e2ab48d64d6662fa0
Jose Mayo <josemayo3@gmail.com>
2024-05-22 19:03:28 -0400

    push task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """checks if object is a subclass instance from a_class"""
-    return issubclass(type(obj), a_class) and not isinstance(obj, a_class)
-
-
+    if type(obj) is a_class:
+        return False
+    return issubclass(type(obj), a_class)
