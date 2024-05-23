15ba1cd0b3264afb31195f01fb94c3f78378cd47
Jose Mayo <josemayo3@gmail.com>
2024-05-22 22:59:51 -0400

    push task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""start of the class"""
-
-
 class MyList(list):
     """MyList class inherited from parent class list"""
+
     def print_sorted(self):
         """prints sorted list"""
         print(sorted(self))
