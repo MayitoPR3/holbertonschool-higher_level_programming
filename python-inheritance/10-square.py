6e394d1ba20e105350f8562a7fed915aaa3edd51
Jose Mayo <josemayo3@gmail.com>
2024-05-22 23:36:34 -0400

    push task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""class named Rectangle that inherits from Rectangle"""
-
-Rectangle = __import__('9-rectangle').Rectangle
+"""Square class Module"""
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """class named Square inherited from Rectangle"""
+    """Square class"""
     def __init__(self, size):
-        super().integer_validator("size", size)
-        super().__init__(size, size)
+        """Method for initializing a square"""
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Method that returns area of a square"""
+        return self.__size ** 2
