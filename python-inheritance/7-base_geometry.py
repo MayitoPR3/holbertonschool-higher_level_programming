cf862a7f103fd81a363ed7b8528d3630f30b25e5
Jose Mayo <josemayo3@gmail.com>
2024-05-22 23:17:35 -0400

    push task 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a class"""
+
+
+class BaseGeometry:
+    """class named BaseGeometry"""
+    def area(self):
+        """returns area of rectangle and handles exception"""
+        raise Exception("area() is mot implemented")
+
+    def integer_validator(self, name, value):
+        """checks if the value is an integer and raises error if not an int"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
