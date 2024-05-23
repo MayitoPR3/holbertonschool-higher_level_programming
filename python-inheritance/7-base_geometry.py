63a5f4714bbe2f5ac6efdfbd4cea0505d546498a
Jose Mayo <josemayo3@gmail.com>
2024-05-22 23:53:03 -0400

    task 7 test

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,14 +4,13 @@
 
 class BaseGeometry:
     """BaseGeometry class"""
+
     def area(self):
         """Method Raises an Exception"""
         raise Exception("area() is not implemented")
 
-    @staticmethod
-    def integer_validator(name, value):
-        """Method for validating value"""
-        if not isinstance(value, int):
+    def integer_validator(self, name, value):
+        if int != type(value):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
