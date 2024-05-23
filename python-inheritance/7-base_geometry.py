c2b8f476c3c2cbd19070ea34d7f6dcf20de40b04
Jose Mayo <josemayo3@gmail.com>
2024-05-22 23:22:25 -0400

    push task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,13 +4,14 @@
 
 class BaseGeometry:
     """class named BaseGeometry"""
+
     def area(self):
-        """returns area of rectangle and handles exception"""
+        """returns area of rectangle and raise exception"""
         raise Exception("area() is mot implemented")
 
     def integer_validator(self, name, value):
         """checks if the value is an integer and raises error if not an int"""
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(name + "must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(name + "must be greater than 0")
