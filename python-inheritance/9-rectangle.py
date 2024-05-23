dd7a3e7b10d27b64e3bc83646b14d1f7b5c212e4
Jose Mayo <josemayo3@gmail.com>
2024-05-22 23:33:54 -0400

    push task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""class named Rectangle that inherits from BaseGeometry"""
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Rectangle class Module"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """class named Rectangle"""
+    """Rectangle class"""
     def __init__(self, width, height):
+        """Initialize rectangle method"""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """returns the area  of the rectangle"""
+        """Method that returns area of rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
-        """returns representation of the Rectangle class"""
-        return "[{}] {}/{}".format("Rectangle", self.__width, self.__height)
+        """Returns a string"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
