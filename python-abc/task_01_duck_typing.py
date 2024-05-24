91a518bc9302f5bec8bcd74d1e8df97e72cc5613
Jose Mayo <josemayo3@gmail.com>
2024-05-24 14:21:19 -0400

    push task 1

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """Class named Shape"""
+    @abstractmethod
+    def area(self):
+        """Defines the area of the class Shape"""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """Defines the perimeter of the class Shape"""
+        pass
+
+
+class Circle(Shape):
+    """Class named Circle with parent class Shape"""
+    def __init__(self, radius):
+        """Initialized the radius"""
+        self.radius = radius
+
+    def area(self):
+        """defines the area of the circle"""
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        """defines the perimeter of the circle"""
+        return math.pi * self.radius * 2
+
+
+class Rectangle(Shape):
+    """class named Rectangle with parent class Shape"""
+    def __init__(self, width, height):
+        """initializes the width and height"""
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """defines the area of the Rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """defines the area of the perimeter"""
+        return (self.width + self.height) * 2
+
+
+def shape_info(shape):
+    """defines the shape information"""
+    print("Area:", shape.area())
+    print(("Perimeter:", shape.perimeter()))
