b5867052d3e9db0ff75a9b2cc166d6deecb23290
Jose Mayo <josemayo3@gmail.com>
2024-05-24 19:12:56 -0400

    task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+class VerboseList(list):
+    """Class named VerboseList that extends the Python list class"""
+    def append(self, item):
+        """defines the override method of list append"""
+        super().append(item)
+        print(f"Added {item} to the list")
+
+    def extend(self, iterable):
+        """defines the override method of list extend"""
+        super().extend(iterable)
+        print(f"Extended the list with {len(iterable)} items")
+
+    def remove(self, item):
+        """defines the override method of list remove"""
+        super().remove(item)
+        print(f"Removed {item} from the list")
+
+    def pop(self, index=None):
+        """defines the override method of list pop"""
+        if index is None:
+            item = super().pop()
+            print(f"Popped {item} from the list.")
+        else:
+            item = super().pop(index)
+            print(f"Popped {item} from the list.")
