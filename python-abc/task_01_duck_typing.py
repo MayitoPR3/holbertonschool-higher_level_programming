bf62e477b3378bb390834bffd9c126dcb820b7f1
Jose Mayo <josemayo3@gmail.com>
2024-05-24 15:02:27 -0400

    push README

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -27,7 +27,7 @@ class Circle(Shape):
 
     def perimeter(self):
         """defines the perimeter of the circle"""
-        return 2 * (math.pi * self.radius)
+        return 2 * math.pi * abs(self.radius)
 
 
 class Rectangle(Shape):
