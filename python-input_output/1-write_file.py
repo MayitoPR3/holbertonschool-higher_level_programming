82ec0a3cdaef895e93aeacd6bd0c33f2f79849d1
MayitoPR3 <josemayo3@gmail.com>
2024-05-28 12:17:24 -0400

    push task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""unction that writes a string to a text file (UTF8)
+and returns the number of characters written"""
+
+
+def write_file(filename="", text=""):
+    """defines the function to write a string to text file"""
+    with open(filename, encoding="utf-8") as file:
+        file.write(text)
+        return len(text)
