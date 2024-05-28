e8d43a4682fbf4c146d562ec2d6b97682a5b4fb7
MayitoPR3 <josemayo3@gmail.com>
2024-05-28 11:31:55 -0400

    push TASK 0 FOR TEST

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
+    """defines the reading of file"""
     with open("my_file_0.txt", encoding="utf-8") as filename:
         print(filename.read())
