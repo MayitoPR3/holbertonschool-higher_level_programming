093c226ba4f4b95c3089b7a22f8f466ae9ef36f2
MayitoPR3 <josemayo3@gmail.com>
2024-05-28 12:32:47 -0400

    test task 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""module for the function that returns
+the JSON representation of an object (string)"""
+
 
 def to_json_string(my_obj):
+    """defines the JSON representation of an object"""
     return my_obj
