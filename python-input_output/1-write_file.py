93cbf4e685e9636c49e6d98cb48346f5355cf061
MayitoPR3 <josemayo3@gmail.com>
2024-05-28 12:20:14 -0400

    fixed typo on module doc

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""unction that writes a string to a text file (UTF8)
+"""function that writes a string to a text file (UTF8)
 and returns the number of characters written"""
 
 
