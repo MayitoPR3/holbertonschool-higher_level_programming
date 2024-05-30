2b836d99001b543d2a10918034bdf0014fccd5f8
Jose Mayo <josemayo3@gmail.com>
2024-05-29 20:26:52 -0400

    tsk 3

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module for converting csv to json"""
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename):
+    """defines the convert of csv to json"""
+    try:
+        with open(csv_filename, 'r', newline='') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+
+            data = list(csv_reader)
+
+            json_data = json.dumps(data, indent=4)
+
+            with open('data.json', 'w') as json_file:
+                json_file.write(json_data)
+            return True
+
+    except FileNotFoundError:
+        print("File not found.")
+        return False
