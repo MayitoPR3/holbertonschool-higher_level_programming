043a4b15dad741ed8052f42fffc1dfa51db4cb73
Jose Mayo <josemayo3@gmail.com>
2024-05-31 15:57:44 -0400

    push task 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    """defines fetch and print"""
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    print("Status Code: ", response.status_code)
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['tittle'])
+
+
+def fetch_and_save_posts():
+    """defines fetch and save"""
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+        keys = ['id', 'title', 'body']
+        with open('posts.csv', 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=keys)
+            writer.writeheader()
+            for post in posts:
+                writer.writerow({key: post[key] for key in keys})
