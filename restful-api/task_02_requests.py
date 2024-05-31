d13cbb592a5889058a1d503280c35a22f30cf593
Jose Mayo <josemayo3@gmail.com>
2024-05-31 18:12:16 -0400

    task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -13,7 +13,7 @@ def fetch_and_print_posts():
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
-            print(post)
+            print(response.status_code)
 
 
 def fetch_and_save_posts():
