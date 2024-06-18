08f30dbf2f9d5b3999f29fd9d46582103cbe650d
Jose Mayo <josemayo3@gmail.com>
2024-06-17 21:03:00 -0400

    push task 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,5 @@
-USE `mysql`;
-
--- Check if the table exists; if not, create it
-CREATE TABLE IF NOT EXISTS `first_table` (
+-- creates a table called first_table in the current database in your MySQL server.
+CREATE TABLE IF NOT EXISTS first_table(
     `id` INT,
     `name` VARCHAR(256)
 );
