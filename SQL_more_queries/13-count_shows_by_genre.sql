68d13f799d6a3dbf296dbff005ae2af8e2fd147b
Jose Mayo <mayitopr@hotmail.com>
2024-06-18 22:56:48 -0400

    push task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
--- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
+-- lists all shows in hbtn_0d_tvshows that have at least 1 genre linked
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
        FROM tv_show_genres
        INNER JOIN tv_genres
