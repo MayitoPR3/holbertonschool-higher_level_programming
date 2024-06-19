2b205f036f27302a7446fcdab008d1f5bf31e1f8
Jose Mayo <mayitopr@hotmail.com>
2024-06-18 22:56:12 -0400

    push task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,4 @@
--- Import the database dump from hbtn_0d_tvshows to your MySQL server: download
--- script that lists all genres from hbtn_0d_tvshows
+-- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
        FROM tv_show_genres
        INNER JOIN tv_genres
