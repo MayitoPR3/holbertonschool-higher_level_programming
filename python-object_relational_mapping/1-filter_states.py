9aa7463a0fb7b3cff894ce4841a36cafd5cfd228
Jose Mayo <josemayo3@gmail.com>
2024-06-21 20:19:59 -0400

    push task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+"""
+    A script that lists all states from the database hbtn_0e_0_usa
+    starting with capital letter N
+    Username, password and database names are given as user args
+"""
+
 
 import sys
 import MySQLdb
