44daf9027cbe32977edb7b4ecd19d5d9470608e9
Jose Mayo <josemayo3@gmail.com>
2024-06-22 03:09:59 -0400

    push

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,12 @@ import sys
 def list_states(username, password, database):
     # Connect to MySQL database
     try:
-        db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+        db = MySQLdb.connect(
+            host='localhost', 
+            port=3306, 
+            user=username, 
+            passwd=password, 
+            db=database)
 
         cursor = db.cursor()
 
