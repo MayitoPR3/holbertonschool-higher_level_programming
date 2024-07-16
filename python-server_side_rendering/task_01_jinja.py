a64067c2271a81c9b0004dba667c3382fb1eccb6
Jose <josemayo3@gmail.com>
2024-07-15 20:08:14 -0400

    push 01

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 # Route for the home page
 @app.route('/')
 def home():
-    return render_template('index.html'), 200
+    return render_template('index.html')
 
 # Route for the about page
 @app.route('/about')
 def about():
-    return render_template('about.html'), 200
+    return render_template('about.html')
 
 # Route for the contact page
 @app.route('/contact')
 def contact():
-    return render_template('contact.html'), 200
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
